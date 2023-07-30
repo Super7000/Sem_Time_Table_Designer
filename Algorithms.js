406beaedf57a6c6ca28fba69c45ef87f8c2367b7
srideep-banerjee <banerjee.srideep@gmail.com>
2023-07-30 13:53:40 +0530

    Removed types dependency

--- Algorithms.js
+++ Algorithms.js
@@ -1,15 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Algorithm = void 0;
-var Util_1 = require("./Util");
-var Algorithm = /** @class */ (function () {
-    function Algorithm(teachers, subjects) {
+const Util_1 = require("./Util");
+class Algorithm {
+    constructor(teachers, subjects) {
         this.teachers = new Map();
         this.subjects = new Map();
         this.isWorking = false;
         if (teachers) {
-            for (var _i = 0, teachers_1 = teachers; _i < teachers_1.length; _i++) {
-                var value = teachers_1[_i];
+            for (let value of teachers) {
                 if (value instanceof Util_1.Teacher) {
                     this.teachers.set(value.name, value);
                 }
@@ -18,8 +17,7 @@ var Algorithm = /** @class */ (function () {
             }
         }
         if (subjects) {
-            for (var _a = 0, subjects_1 = subjects; _a < subjects_1.length; _a++) {
-                var value = subjects_1[_a];
+            for (let value of subjects) {
                 if (value instanceof Util_1.Subject) {
                     this.subjects.set(value.subjectCode, value);
                 }
@@ -28,24 +26,21 @@ var Algorithm = /** @class */ (function () {
             }
         }
     }
-    Algorithm.prototype.addTeachers = function (teachers, subjects) {
-        for (var _i = 0, teachers_2 = teachers; _i < teachers_2.length; _i++) {
-            var value = teachers_2[_i];
+    addTeachers(teachers, subjects) {
+        for (let value of teachers) {
             if (value instanceof Util_1.Teacher) {
                 this.teachers.set(value.name, value);
             }
             else
                 throw new TypeError("elements of \'teachers\' must be objects of class Teacher");
         }
-        for (var _a = 0, subjects_2 = subjects; _a < subjects_2.length; _a++) {
-            var value = subjects_2[_a];
+        for (let value of subjects) {
             if (value instanceof Util_1.Subject) {
                 this.subjects.set(value.subjectCode, value);
             }
             else
                 throw new TypeError("elements of \'subjects\' must be objects of class Subject");
         }
-    };
-    return Algorithm;
-}());
+    }
+}
 exports.Algorithm = Algorithm;
