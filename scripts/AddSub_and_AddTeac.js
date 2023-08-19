7a073cb440708f9842adfbc02d506b94e62f906a
Super7000 <subratachowdhury7000@gmail.com>
2023-08-19 21:45:22 +0530

    some new files added

---
+++ scripts/AddSub_and_AddTeac.js
@@ -0,0 +1,19 @@
+
+document.querySelectorAll(".card").forEach((t) => {
+    t.addEventListener("click", () => {
+        if (t != document.querySelector(".card.active")) {
+            document.querySelector(".card.active").classList.remove("active");
+            t.classList.add("active");
+        }
+    })
+});
+
+document.querySelector(".add.card").addEventListener("click",()=>{
+    document.querySelectorAll(".t_d .con input")[0].focus();
+});
+
+document.querySelectorAll(".cards .d_card").forEach((e)=>{
+    e.addEventListener("click",()=>{
+        document.querySelectorAll(".t_d .con input")[0].value = e.innerHTML;
+    })
+})
