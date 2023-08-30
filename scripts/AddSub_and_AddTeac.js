5cc5a61ac4a67b73b1e1fbe25f3adc28e9708232
Super7000 <subratachowdhury7000@gmail.com>
2023-08-30 22:23:31 +0530

    Added error popup and confimation popup window

--- scripts/AddSub_and_AddTeac.js
+++ scripts/AddSub_and_AddTeac.js
@@ -12,11 +12,27 @@ document.querySelector(".add.card").addEventListener("click",()=>{
     document.querySelectorAll(".t_d .con input").forEach((e)=>{
         e.value = "";
     });
+    document.querySelector(".btn_con .ddb").style.display = "none";
     document.querySelectorAll(".t_d .con input")[0].focus();
 });
 
 document.querySelectorAll(".cards .d_card").forEach((e)=>{
     e.addEventListener("click",()=>{
         document.querySelectorAll(".t_d .con input")[0].value = e.innerHTML;
+        document.querySelector(".btn_con .ddb").style.display = "block";
     })
 })
+
+// toggle confirmation box function
+function tconfirmationbox(cmsg){
+    document.querySelector(".cBox .cMsg").innerHTML = cmsg;
+    document.querySelector(".cBox").classList.toggle("active");
+    document.querySelector(".cBoxBG").classList.toggle("active");
+}
+window.onload = function(){
+    document.onclick = function(e){
+        if(e.target.classList[0]=="cBoxBG" || e.target.classList[1] == "btn" || e.target.classList == "ddb"){
+            tconfirmationbox(`Are you really want to delete ${document.querySelector(".con:nth-child(2) input").value}?`);
+        }
+    };
+  };
